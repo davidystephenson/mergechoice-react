cad510cdad90fab57fd35eb4ab4396cb0af61b0c
David Y. Stephenson <david@davidystephenson.com>
2023-10-07 21:31:47 -0400

    first commit

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface Item {
+  id: string
+  label: string
+  points: number
+}
+export interface Operation {
+  input: string[][]
+  output: string[]
+  steps: number
+}
+export interface Choice {
+  options: string[]
+  currentOperationIndex: number
+  leftIndex: number
+  rightIndex: number
+}
+export interface State {
+  items: Item[]
+  operations: Operation[]
+  choice: Choice
+  finalized: boolean
+}
